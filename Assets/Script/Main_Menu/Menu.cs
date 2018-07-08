25356a8fe032f0d37daa1d0078d6c291b5e4c4c0
labroche <corentin.labroche@inist.fr>
2018-07-08 21:37:58 +0200

    [PATCH] Fix menu and add controls with another key

--- Assets/Script/Main_Menu/Menu.cs
+++ Assets/Script/Main_Menu/Menu.cs
@@ -1,40 +1,65 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
-  public GameObject bar1;
-  public GameObject bar2;
-  public bool barA;
-  public bool barB;
+  public GameObject[] bars;
+  public bool change;
+  public int currentItem;
   public Scene Level;
   // Use this for initialization
   void Start () {
-    bar2.gameObject.SetActive (false);
+    currentItem = 0;
+    updateSelectedItem ();
   }
 
   // Update is called once per frame
   void Update () {
-    if (Input.GetKeyDown ("s")) {
-      bar1.gameObject.SetActive (false);
-      barA = (false);
-      bar2.gameObject.SetActive (true);
-      barB = (true);
-    }
-    if (Input.GetKeyDown ("z")) {
-      bar2.gameObject.SetActive (false);
-      barB = (false);
-      bar1.gameObject.SetActive (true);
-      barA = (true);
-    }
-    if (Input.GetKeyDown ("space")) {
-      if (barA == true) {
-        print ("play");
-        SceneManager.LoadScene ("DebugZone");
+    if (Input.GetKeyDown ("z") || Input.GetKeyDown ("up")) {
+      if (currentItem > 0) {
+
+        currentItem -= 1;
+        updateSelectedItem ();
       }
-      if (barB == true) {
-        print ("quit");
+    }
+    if (Input.GetKeyDown ("s") || Input.GetKeyDown ("down")) {
+      if (currentItem < bars.Length - 1) {
+        currentItem += 1;
+        updateSelectedItem ();
+      }
+    }
+    if (Input.GetKeyDown ("space") || Input.GetKeyDown (KeyCode.KeypadEnter) || Input.GetKeyDown (KeyCode.Return)) {
+      String selected = bars[currentItem].GetComponent<Item> ().itemName;
+      switch (selected) {
+        case "play":
+          SceneManager.LoadScene ("DebugZone");
+          break;
+        case "quit":
+          print ("quit");
+          break;
+        default:
+          break;
+      }
+
+      // if (barA == true) {
+      //   print ("play");
+      //   
+      // }
+      // if (barB == true) {
+      //   print ("quit");
+      // }
+    }
+  }
+
+  void updateSelectedItem () {
+    for (int i = 0; i < bars.Length; i++) {
+      GameObject item = bars[i];
+      if (i != currentItem) {
+        item.gameObject.SetActive (false);
+      } else {
+        bars[currentItem].gameObject.SetActive (true);
       }
     }
   }
