c4307bfbbae9e52e6cdb5f056303992e5412778b
labroche <corentin.labroche@inist.fr>
2018-07-10 21:12:16 +0200

    [PATCH] Make shoot more smooth

--- Assets/Script/Shoot.cs
+++ Assets/Script/Shoot.cs
@@ -8,6 +8,7 @@ public class Shoot : MonoBehaviour {
   public bool bouge = false;
 
   void Start () {
+    speed = 10.0f;
     if (gameObject.name.Contains ("(Clone)")) {
       Destroy (gameObject, 1);
     }
@@ -15,7 +16,7 @@ public class Shoot : MonoBehaviour {
 
   void Update () {
     if (gameObject.name.Contains ("(Clone)")) {
-      transform.Translate (new Vector2 (0, 0.1f));
+      transform.Translate (Vector3.up * Time.deltaTime * speed);
     }
   }
   void OnCollisionEnter2D (Collision2D col) {
