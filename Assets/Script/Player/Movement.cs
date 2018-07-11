ecc3e4c209cfd838d5f8020bdef574d24e9450a5
clabroche <corentinlabroche@gmail.com>
2018-07-11 14:40:58 +0000

    [MINOR] can Kill an enemy

--- Assets/Script/Player/Movement.cs
+++ Assets/Script/Player/Movement.cs
@@ -3,39 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-  public float x;
-  public float y;
-  public float force;
   public GameObject player;
   public GameObject shoot;
-  public Transform playerTransform;
-  private Rigidbody2D rb;
-
+  public GameObject enemy1;
   void Start () {
     Cursor.visible = false;
+    // StartCoroutine(enemy1.GetComponent<Enemy1> ().StartSpawner());
+    enemy1.GetComponent<Enemy1> ().Spawn();
+
   }
 
   void Update () {
     if (Input.GetButton ("up")) {
-       transform.position += transform.up * 2.0f * Time.deltaTime;
+      transform.position += transform.up * 2.0f * Time.deltaTime;
     }
 
     if (Input.GetButton ("down")) {
-       transform.position += transform.up * -2.0f * Time.deltaTime;
+      transform.position += transform.up * -2.0f * Time.deltaTime;
     }
 
     if (Input.GetButton ("right")) {
-      transform.Rotate (0, 0, -force);
+      transform.Rotate (0, 0, 2.0f);
     }
 
     if (Input.GetButton ("left")) {
-      transform.Rotate (0, 0, force);
+      transform.Rotate (0, 0, 2.0f);
     }
 
     if (Input.GetKeyDown ("space")) {
-      GameObject newShoot = Instantiate (shoot, new Vector2(0,0), player.transform.rotation);
-      newShoot.transform.position = new Vector2(player.transform.position.x, player.transform.position.y);
-      newShoot.transform.Translate(new Vector2(0, 0.7f));
+      GameObject newShoot = Instantiate (shoot, new Vector2 (0, 0), player.transform.rotation);
+      newShoot.transform.position = new Vector2 (player.transform.position.x, player.transform.position.y);
+      newShoot.transform.Translate (new Vector2 (0, 0.7f));
     }
   }
 }
